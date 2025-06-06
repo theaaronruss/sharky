8472f6fd831868a933f6e74d6b267362082a12e1
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-06-05 23:43:05 -0400

    Add function for retrieving a default DSN to use when device isn't specified

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type (
+	Config struct {
+		DefaultDsn string `json:"defaultDsn"`
+	}
+)
+
+var userConfigDir, _ = os.UserConfigDir()
+var configDir = userConfigDir + "/sharky/"
+var configFile = configDir + "config.json"
+
+func GetDefaultDsn() (string, error) {
+	filesBytes, err := os.ReadFile(configFile)
+	if err != nil {
+		return "", fmt.Errorf("Failed to read config file: %w", err)
+	}
+	var config Config
+	err = json.Unmarshal(filesBytes, &config)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse config file: %w", err)
+	}
+	return config.DefaultDsn, nil
+}
