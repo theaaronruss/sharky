cee78ced2b6e037f7e10bd134168a53414149f0d
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-06-05 21:04:02 -0400

    Add function for retrieving latest datapoint on a property

--- api/properties.go
+++ api/properties.go
@@ -2,9 +2,12 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type (
@@ -12,6 +15,12 @@ type (
 		Name string
 		Dsn string
 		Value string
+	}
+
+	getDatapointResponse struct {
+		Datapoint struct {
+			Value json.RawMessage `json:"value"`
+		} `json:"datapoint"`
 	}
 )
 
@@ -65,4 +74,32 @@ func UpdateProperty(accessToken string, userUuid string, dsn string, name string
 		return errors.New("Invalid response while adding property: http status: " + fmt.Sprint(response.StatusCode))
 	}
 	return nil
+}
+
+func GetLatestDatapointValue(accessToken string, dsn string, name string) (string, error) {
+	request, err := http.NewRequest(http.MethodGet, "https://ads-field-39a9391a.aylanetworks.com/apiv1/dsns/" + dsn + "/properties/" + name + "/datapoints.json?limit=1", nil)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create request to get datapoint: %w", err)
+	}
+	request.Header.Add("Authorization", accessToken)
+	request.Header.Add("Content-Type", "application/json")
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get datapoint: %w", err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode == 401 {
+		return "", errors.New("Invalid response while getting datapoint: not authorized")
+	} else if response.StatusCode == 404 {
+		return "", errors.New("Invalid response while getting datapoint: invalid DSN or property name")
+	}
+	var datapoints []getDatapointResponse
+	responseBytes, _ := io.ReadAll(response.Body)
+	json.Unmarshal(responseBytes, &datapoints)
+	if len(datapoints) == 0 {
+		return "", errors.New("No datapoints found for property")
+	}
+	value := string(datapoints[0].Datapoint.Value)
+	value = strings.ReplaceAll(value, "\"", "")
+	return string(value), nil
 }
